9f0d815746eea920a80aaae52ea62e7ba2ada9c5
alex1431999 <ah01242@surrey.ac.uk>
2019-11-10 16:30:40 +0000

    Edited crawler search function
    Renamed function
    
    Now returns dicts instead of Twitter Crawl objects

--- crawler.py
+++ crawler.py
@@ -25,29 +25,39 @@ class TwitterCrawler():
 
         self.api = tweepy.API(auth)
 
-    def search_keyword(self, keyword_string, language, limit=sys.maxsize):
+    def search(self, keyword, limit=sys.maxsize):
         """
         Search twitter for specific keyword and return just the text
 
-        :param str keyword_string: The target keyword that should be searched for
-        :param str language: The language that should be used for the search
+        :param Keyword keyword: The target keyword that should be searched for
         :param int limit: The amount of tweets you would like to have returned at most
         """
-        if (language not in SUPPORTED_LANGUAGES):
-            raise Exception('Unsupported language "{}"'.format(language))
+        if (keyword.language not in SUPPORTED_LANGUAGES):
+            raise Exception('Unsupported language "{}"'.format(keyword.language))
 
-        tweets = self.api.search(keyword_string, tweet_mode='extended', count=limit, lang=language, include_entities=True) # For now default to english
+        tweets = self.api.search(keyword.keyword_string, tweet_mode='extended', count=limit, lang=keyword.language, include_entities=True)
+
         twitter_results = []
         for tweet in tweets:
             likes = tweet.favorite_count
             retweets = tweet.retweet_count
-            id = tweet.id
+            tweet_id = tweet.id
+            timestamp = tweet.created_at
+
             try: # Sometimes the tweet is a retweet
                 text = tweet.retweeted_status.full_text
-                twitter_result = TwitterResult(id, keyword_stringm, language, text, likes=likes, retweets=retweets)
-                twitter_results.append(twitter_result)
             except: # If it's not, then just add the normal tweet text
                 text = tweet.full_text
-                twitter_result = TwitterResult(id, keyword_string, language, text, likes=likes, retweets=retweets)
-                twitter_results.append(twitter_result)
+            
+            # Package the tweet
+            twitter_result = {
+                'keyword_id': keyword._id,
+                'tweet_id': tweet_id,
+                'text': text,
+                'likes': likes,
+                'retweets': retweets,
+                'timestamp': timestamp,
+            }
+
+            twitter_results.append(twitter_result)
         return twitter_results
