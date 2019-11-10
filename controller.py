a212fc45690c2228ba6dc196f98b84951f4e8278
alex1431999 <ah01242@surrey.ac.uk>
2019-11-10 17:40:50 +0000

    Added controller performing basic functionality
    Grabbing all keywords from the database and getting the corresponding tweets

---
+++ controller.py
@@ -0,0 +1,59 @@
+"""
+Module to hanlde different scripts that the crawler can execute
+"""
+
+import bson
+
+from common.mongo.controller import MongoController
+from common.mongo.data_types.keyword import Keyword
+
+from crawler import TwitterCrawler
+
+class Controller():
+    """
+    Hanldes the different scripts and order of execution
+    """
+    def __init__(self):
+        """
+        Initialise the crawler
+        """
+        self.crawler = TwitterCrawler()
+        self.mongo_controller = MongoController()
+
+    def __save_tweet(self, twitter_result):
+        """
+        Save a twitter result to the MongoDb
+
+        :param dict twitter_result: The result received packaged up
+        """
+        return self.mongo_controller.add_crawl_twitter(
+            twitter_result['keyword_id'],
+            twitter_result['tweet_id'],
+            twitter_result['text'],
+            twitter_result['likes'],
+            twitter_result['retweets'],
+            twitter_result['timestamp'],
+        )
+
+    def __save_tweets(self, twitter_results):
+        for twitter_result in twitter_results:
+            self.__save_tweet(twitter_result)
+
+    def run_full(self):
+        """
+        Get all outdated keywords and run a crawl over them
+        """
+        # Get a cursor of all the keywords in the databse
+        keyword_cursor = self.mongo_controller.get_keyword_batch_cursor()
+
+        # Go over each batch
+        for batch in keyword_cursor:
+
+            # Go over each keyword in the batch
+            for keyword_dict in bson.decode_all(batch):
+
+                keyword = Keyword(keyword_dict) # Cast the keyword to a Keyword object
+                twitter_results = self.crawler.search(keyword, limit=10) # Run the search
+                self.__save_tweets(twitter_results) # Save all tweets to the DB
+
+        
---
+++ run.py
@@ -0,0 +1,22 @@
+"""
+Accepts input commands and runs scripts accordingly
+"""
+
+import sys
+
+from controller import Controller
+
+def run(mode):
+    # Create a new controller instance
+    controller = Controller()
+
+    # Run the specified mode
+    if mode == 'full':
+        controller.run_full()
+    else:
+        print('Please enter a valid mode')
+
+
+if __name__ == '__main__':
+    mode = sys.argv[1]
+    run(mode)
