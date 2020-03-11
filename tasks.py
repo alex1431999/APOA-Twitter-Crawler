f6c2082ce2eac8472024acf9c7285a1632cc40e4
alex1431999 <ah01242@surrey.ac.uk>
2020-03-11 14:18:32 +0000

    Use custom exceptions

--- tasks.py
+++ tasks.py
@@ -4,7 +4,9 @@ It also runs all the setup required for celery to function.
 """
 import os
 
+from common.exceptions.parameters import InvalidParameterError, UnsupportedLanguageError
 from common.utils.logging import DEFAULT_LOGGER, LogTypes
+from common.config import SUPPORTED_LANGUAGES
 from common.celery import queues
 from celery import Celery
 
@@ -22,6 +24,10 @@ def crawl_twitter_keyword(keyword_string, language):
     :param str keyword_string: The target keyword string
     :param str language: The target language
     """
+    # Validation
+    if keyword_string is None or keyword_string == '': raise InvalidParameterError(keyword_string)
+    if language not in SUPPORTED_LANGUAGES: raise UnsupportedLanguageError(language)
+
     controller = Controller()
     DEFAULT_LOGGER.log('Received twitter keyword crawl request for {} ({})'.format(keyword_string, language), log_type=LogTypes.INFO.value)
     result = controller.run_single_keyword(keyword_string, language)
